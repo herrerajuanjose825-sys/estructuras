749108aac5195f63858942db240c6e4fa5254620
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:18:46 -0500

    suma de los caracteres

---
+++ holamundo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class holamundo {
+ public static void main (String[]args){
+    System.out.println ("hola mundo");
+    int n1=0, n2=0;
+    Scanner sc = new Scanner (System.in);
+    System.out.println("ingrese el numero 1");
+    n1= sc.nextInt();
+    System.out.println("ingrese el numero 2");
+    n2= sc.nextInt();
+    System.out.println ("la suma de los numeros es "+ (n1  +  n2 ));
+   
+ } 
+
+}
